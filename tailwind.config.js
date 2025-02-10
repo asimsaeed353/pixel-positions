c9af3150a93c066ab9879fb301aff7a49ba9d328
vuswh <vuswh@LRO-SH-19.vu.edu.pk>
2025-02-10 15:17:31 +0500

    Pixel Positions Job Posting Site
    We are done with HTML and CSS part of this website. Now, we'll focus on building the architecture of our website

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+    ],
+    theme: {
+        extend: {
+            colors: {
+              "black" : "#060606"
+            },
+            fontFamily: {
+                "hanken-grotesk" : ["Hanken Grotesk", "sans-serif"]
+            },
+            fontSize: {
+                "2xs" : ".635rem", // 10px
+            }
+        },
+    },
+    plugins: [],
+};
